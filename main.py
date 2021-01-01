7d5c498bdbed462f974b8a76c5d4821f1e8fbf3a
Slawek Zachcial <slawek.zachcial@gmail.com>
2021-01-01 21:32:31 +0100

    Hardcode nesting for 4-order hexagon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import sys
 import itertools
 
 HEXAGON_SIDES = 6
@@ -45,42 +46,90 @@ def rings(sideSums, candidates):
 def ringToList(ring):
     return sum(map(lambda l: l[0:-1], ring), [])
 
+def sideSize(cellCount):
+    countCells = lambda n: 3*n*n-3*n+1
+    n = next(x for x in itertools.count(1) if countCells(x) >= cellCount)
+    if cellCount == countCells(n):
+        return n
+    raise ValueError(f'Unable to find n satisfying 3*n*n-3*n+1=={cellCount}')
+
 def ringIndicesToLinear(ringList):
     if len(ringList) == 1:
         return ringList
     # TODO: there must be a better way to do this!
-    if len(ringList) == 7:
-        return list(map(lambda i: ringList[i], [0,1,5,6,2,4,3]))
-    if len(ringList) == 19:
-        return list(map(lambda i: ringList[i], [0,1,2,11,12,13,3,10,17,18,14,4,9,16,15,5,8,7,6]))
-    return []
+    indicesForLength = {
+        7: [0,1,5,6,2,4,3],
+        19: [0,1,2,11,12,13,3,10,17,18,14,4,9,16,15,5,8,7,6],
+        37: [0,1,2,3,17,18,19,20,4,16,29,30,31,21,5,15,18,35,36,32,22,6,14,27,34,33,23,7,13,26,25,24,8,12,11,10,9]
+    }
+    if len(ringList) in indicesForLength:
+        return list(map(lambda i: ringList[i], indicesForLength[len(ringList)]))
+    raise ValueError(f'Unable to remap indices for list of size {len(ringList)}')
+
+def collectResults(results, current, sums, possibleNumbers, sideSize):
+    if sideSize == 1:
+        # if we get here - possibleNumbers is a 1-element set
+        results += [current + list(possibleNumbers)]
+        return
+
+    possibleRings = rings(sums, list(filter(lambda x: sum(x) in sums,
+                                            map(list, itertools.permutations(possibleNumbers, sideSize)))))
+
+    for ring in possibleRings:
+        ringNumbers = set(sum(ring, []))
+        innerRingPossibleNumbers = possibleNumbers - ringNumbers
+        ringSums = sumsForRing(ring)
+        innerRingSums = list(map(lambda i: sums[i] - ringSums[i], range(HEXAGON_SIDES)))
+
+        collectResults(results, current + ringToList(ring), innerRingSums, innerRingPossibleNumbers, sideSize - 1)
 
 if __name__ == '__main__':
-    SIDE_SIZE = 3
-    SIDE_SUM = 38
-    NUMBERS = range(1, 20)
+    SIDE_SIZE = 3 if len(sys.argv) == 1 else int(sys.argv[1])
+    SIDE_SUM = 38 if len(sys.argv) == 1 else int(sys.argv[2])
+    rangeFrom = 1 if len(sys.argv) == 1 else int(sys.argv[3])
+    rangeTo =  19 if len(sys.argv) == 1 else int(sys.argv[4])
+    NUMBERS = range(rangeFrom, rangeTo+1)
 
-    result = []
+    results = []
 
     outerRingSums = [SIDE_SUM] * HEXAGON_SIDES
+    outerRingPossibleNumbers = set(NUMBERS)
 
-    outerRings = rings(outerRingSums,
-                       list(filter(lambda x: sum(x) in outerRingSums,
-                                   map(list, itertools.permutations(NUMBERS, SIDE_SIZE)))))
+    # collectResults(results, [], outerRingSums, outerRingPossibleNumbers, SIDE_SIZE)
 
-    for outerRing in outerRings:
-        outerRingNumbers = set(sum(outerRing, []))
-        innerRingNumbers = set(NUMBERS) - outerRingNumbers
-        innerRingSums = list(map(lambda v: SIDE_SUM - v, sumsForRing(outerRing)))
+    ringL0Sums = [SIDE_SUM] * HEXAGON_SIDES
+    ringL0PossibleNumbers = set(NUMBERS)
 
-        innerRings = rings(innerRingSums,
-                           list(filter(lambda x: sum(x) in innerRingSums,
-                                       map(list, itertools.permutations(innerRingNumbers, SIDE_SIZE-1)))))
+    ringsL0 = rings(ringL0Sums,
+                    list(filter(lambda x: sum(x) in ringL0Sums,
+                                map(list, itertools.permutations(ringL0PossibleNumbers, SIDE_SIZE)))))
 
-        for innerRing in innerRings:
-            finalNumber = list(innerRingNumbers - set(sum(innerRing, [])))
+    for ringL0 in ringsL0:
+        ringL0Numbers = set(sum(ringL0, []))
+        ringL1PossibleNumbers = ringL0PossibleNumbers - ringL0Numbers
+        sumsForRingL0 = sumsForRing(ringL0)
+        ringL1Sums = list(map(lambda i: ringL0Sums[i] - sumsForRingL0[i], range(HEXAGON_SIDES)))
 
-            result += [ringToList(outerRing) + ringToList(innerRing) + finalNumber]
+        ringsL1 = rings(ringL1Sums,
+                        list(filter(lambda x: sum(x) in ringL1Sums,
+                                    map(list, itertools.permutations(ringL1PossibleNumbers, SIDE_SIZE-1)))))
 
+        for ringL1 in ringsL1:
+            if SIDE_SIZE == 3:
+                finalNumberL2 = list(ringL1PossibleNumbers - set(sum(ringL1, [])))
+                results += [ringToList(ringL0) + ringToList(ringL1) + finalNumberL2]
+            elif SIDE_SIZE == 4:
+                ringL1Numbers = set(sum(ringL1, []))
+                ringL2PossibleNumbers = ringL1PossibleNumbers - ringL1Numbers
+                sumsForRingL1 = sumsForRing(ringL1)
+                ringL2Sums = list(map(lambda i: ringL1Sums[i] - sumsForRingL1[i], range(HEXAGON_SIDES)))
 
-    for r in result: print(ringIndicesToLinear(r))
+                ringsL2 = rings(ringL2Sums,
+                                list(filter(lambda x: sum(x) in ringL2Sums,
+                                            map(list, itertools.permutations(ringL2PossibleNumbers, SIDE_SIZE-2)))))
+
+                for ringL2 in ringsL2:
+                    finalNumberL3 = list(ringL2PossibleNumbers - set(sum(ringL2, [])))
+                    results += [ringToList(ringL0) + ringToList(ringL1) + ringToList(ringL2) + finalNumberL3]
+
+    for r in results: print(ringIndicesToLinear(r))
--- main_test.py
+++ main_test.py
@@ -58,12 +58,20 @@ class MainTests(unittest.TestCase):
         self.assertEqual(ringToList([[1,2],[2,3],[3,1]]),
                          [1,2,3])
 
+    def test_sideSize(self):
+        self.assertEqual(sideSize(1), 1)
+        self.assertEqual(sideSize(7), 2)
+        self.assertEqual(sideSize(19), 3)
+        self.assertEqual(sideSize(37), 4)
+        self.assertRaises(ValueError, sideSize, 5)
+
     def test_ringIndicesToLinear(self):
         self.assertEqual(ringIndicesToLinear([7]), [7])
         self.assertEqual(ringIndicesToLinear([1, 2, 3, 4, 5, 6, 7]),
                          [1, 2, 6, 7, 3, 5, 4])
         self.assertEqual(ringIndicesToLinear([1,2,3,7,12,16,19,18,17,13,8,4,5,6,11,15,14,9,10]),
                          [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19])
+        self.assertRaises(ValueError, ringIndicesToLinear, [1, 2])
 
 if __name__ == '__main__':
     unittest.main()
